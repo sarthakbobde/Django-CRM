3f9b72f8413e5bbcfce43cd72e2816e0926aa28a
Sarthak <bobdesarthak@gmail.com>
2024-12-24 01:27:51 +0530

    Records

--- website/views.py
+++ website/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
 from django.contrib import messages
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+    
     # checking login
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +23,7 @@ def home(request):
             messages.success(request, "There was as error while logging in")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 def logout_user(request):
     logout(request)
@@ -27,4 +31,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Sucessfully Registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+        
+    return render(request, 'register.html', {'form':form})
